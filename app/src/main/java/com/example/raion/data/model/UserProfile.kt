73e2268880ef29cb62fec58ff233257c411cfbeb
divarvian <divarvian@gmail.com>
2026-03-01 17:53:04 +0700

    fix: add username

--- app/src/main/java/com/example/raion/data/model/UserProfile.kt
+++ app/src/main/java/com/example/raion/data/model/UserProfile.kt
@@ -7,6 +7,8 @@ import kotlinx.serialization.Serializable
 data class UserProfile(
     @SerialName("id")
     val id: String,
+    @SerialName("username")
+    val username: String,
     @SerialName("name")
     val name: String,
     @SerialName("birth_date")
